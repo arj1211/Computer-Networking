e568737f2f32643823b801f4d9c019e8529a00eb
Arjun Bawa <a3bawa@uwaterloo.ca>
2020-05-31 14:24:35 -0400

    added departure gen + service time gen helper

--- Lab 1/DES.py
+++ Lab 1/DES.py
@@ -30,3 +30,27 @@ def gen_arrivals(rate):
         arrival_events.append({'time':t,'type':'arrival'})
         t += expn_random(rate)
     return arrival_events
+
+# Create departure events
+def gen_departures(arrival_events):
+    prev_d_time = 0
+    departure_events = []
+    for ae in arrival_events:
+        # pkt_len = expn_random(1/AVG_PKT_LEN)
+        # service_time = pkt_len/TRANS_RATE
+        service_time = gen_service_time()
+        d_time = 0
+        if (ae['time'] > prev_d_time):
+            ''' if the current pkt arrived after the previous pkt 
+                already left (ie. the queue is empty/idle) '''
+            d_time = ae['time']+service_time
+        else: d_time = prev_d_time + service_time
+        departure_events.append({'time':d_time,'type':'departure'})
+        prev_d_time = d_time
+    return departure_events
+
+# Generate service time
+def gen_service_time():
+    pkt_len = expn_random(1/AVG_PKT_LEN)
+    service_time = pkt_len/TRANS_RATE
+    return service_time
