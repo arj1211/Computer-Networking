a0640810e29860663e2e139a3800c8fc280e8d9d
Arjun Bawa <a3bawa@uwaterloo.ca>
2020-05-31 14:18:23 -0400

    added imports

--- Lab 1/DES.py
+++ Lab 1/DES.py
@@ -1,3 +1,7 @@
+import random
+import math
+import csv
+
 TRANS_RATE = 1E6 # C := transmission rate of output link in bps, default=1Mbps
 SIM_TIME = 1000 # T := simulation time in seconds
 AVG_PKT_LEN = 2000 # L := avg len of pkt in bits
