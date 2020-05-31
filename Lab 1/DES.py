2e42ef8f76510ece13fbae8685fa7639e1fb95c5
Arjun Bawa <a3bawa@uwaterloo.ca>
2020-05-31 14:21:11 -0400

    added random number gen function

--- Lab 1/DES.py
+++ Lab 1/DES.py
@@ -5,3 +5,10 @@ import csv
 TRANS_RATE = 1E6 # C := transmission rate of output link in bps, default=1Mbps
 SIM_TIME = 1000 # T := simulation time in seconds
 AVG_PKT_LEN = 2000 # L := avg len of pkt in bits
+
+# Random exponentially distributed number generator
+def expn_random(rate):
+    u = random.uniform(0, 1)
+    x = -(1/rate) * math.log(1 - u)
+    return x
+
