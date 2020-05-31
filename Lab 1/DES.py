37d1df23a83adee80716d87297c6eac9f2b29faa
Arjun Bawa <a3bawa@uwaterloo.ca>
2020-05-31 14:23:18 -0400

    added packet arrival and observer event functions

--- Lab 1/DES.py
+++ Lab 1/DES.py
@@ -12,3 +12,21 @@ def expn_random(rate):
     x = -(1/rate) * math.log(1 - u)
     return x
 
+# Create observer events
+def gen_observers(arrival_rate):
+    observer_events = []
+    t = expn_random(arrival_rate*(5+random.uniform(0,2)))
+    while t < SIM_TIME:
+        observer_events.append({'time':t,'type':'observation'})
+        t += expn_random(arrival_rate*(5+random.uniform(0,2)))
+    return observer_events
+
+# Create arrival events
+def gen_arrivals(rate):
+    arrival_events = []
+    t = expn_random(rate) # time of first pkt arrival
+    while t < SIM_TIME:
+        # p = Packet(pkt_size=0,arrival_time=t,service_time=0,departure_time=0)
+        arrival_events.append({'time':t,'type':'arrival'})
+        t += expn_random(rate)
+    return arrival_events
