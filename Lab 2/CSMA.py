095e92feb0b49b52a7cc899612f38b74267ef846
Arjun Bawa <a3bawa@uwaterloo.ca>
2020-06-17 13:39:48 -0400

    functions: transmitting node, times bus is busy

--- Lab 2/CSMA.py
+++ Lab 2/CSMA.py
@@ -15,6 +15,13 @@ L = 5
 D = 100
 # Propagation speed
 S = 25
+# Propagation delay
+p_delay = D/S
+# transmission time
+trans_time = L/R
+
+# max number of transmission retries
+K_max = 10
 
 T_sim = 1000
 
@@ -31,17 +38,48 @@ def gen_arrivals(rate):
         t += expn_random(rate)
     return arrival_events
 
+# next transmitting node
+def determine_next_node(Q):
+    # find first sending node
+    mint = Q[0][0]
+    firstnode = 0
+    for i in range(len(Q)):
+        if mint > Q[i][0]:
+            firstnode = i
+            mint = Q[i][0]
+    return firstnode
 
-# Do for multiple T_sims
-while T_sim <= 3000:
+# calculate at what times all the nodes will consider the bus busy
+def busy_bus_times(sender, pkt_timestamp, num_nodes):
+    n = []
+    for i in range(num_nodes):
+        starttime = pkt_timestamp+abs(i-sender)*p_delay
+        endtime = starttime + trans_time
+        n.append((starttime,endtime))
+    return n
+
+# not sure if this is right
+def expn_backoff_time(tries):
+    return random.uniform(0,2**tries-1)*512/R
+
+def persistent_sim():
     # Set up queues for each of N nodes
     Q = [gen_arrivals(A) for i in range(N)]
+    
     # find first sending node
     mint = Q[0][0]
     firstnode = 0
     for i in range(N):
         if mint > Q[i][0]:
-            node = i
+            firstnode = i
             mint = Q[i][0]
-    # need to put this into functions instead...
+    print(firstnode)
+    # firstnode is sender. Calculate time 
+    # for pkt from firstnode to reach
+    # every other node
+
+
+# Do for multiple T_sims
+while T_sim <= 3000:
+    print(persistent_sim())
     T_sim+=1000
