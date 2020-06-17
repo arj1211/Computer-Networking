c246c7d65885e6cb37933623066041a2c9ada659
Arjun Bawa <a3bawa@uwaterloo.ca>
2020-06-17 10:58:46 -0400

    prototyping 1-persistent

--- Lab 2/CSMA.py
+++ Lab 2/CSMA.py
@@ -1,17 +1,20 @@
+from math import log
+import random
+
 # The number of nodes/computers connected to the LAN (variable).
-N = 1
+N = 10
 # Average packet arrival rate (packets/second) (variable). 
 # Data packets arrive at the MAC layer following a Poisson 
 # process at all nodes.
-A = 1
+A = 2
 # The speed of the LAN/channel/bus (fixed).
-R = 1
+R = 20
 # Packet length (fixed).
-L = 1
+L = 5
 # Distance between adjacent nodes on the bus/channel.
-D = 1
+D = 100
 # Propagation speed
-S = 1
+S = 25
 
 T_sim = 1000
 
@@ -19,3 +22,26 @@ T_sim = 1000
 def expn_random(rate):
     return (-(1/rate) * log(1 - random.uniform(0, 1)))
 
+# Create arrival events
+def gen_arrivals(rate):
+    arrival_events = []
+    t = expn_random(rate) # time of first pkt arrival
+    while t < T_sim:
+        arrival_events.append(t)
+        t += expn_random(rate)
+    return arrival_events
+
+
+# Do for multiple T_sims
+while T_sim <= 3000:
+    # Set up queues for each of N nodes
+    Q = [gen_arrivals(A) for i in range(N)]
+    # find first sending node
+    mint = Q[0][0]
+    firstnode = 0
+    for i in range(N):
+        if mint > Q[i][0]:
+            node = i
+            mint = Q[i][0]
+    # need to put this into functions instead...
+    T_sim+=1000
